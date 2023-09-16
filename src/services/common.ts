b085f1027df00251b8fabd78a0b50f97fd2d7020
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-16 20:16:04 +0900

    API通信の追加

---
+++ src/services/common.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+
+export const getTestRequest = async() => {
+  if (API_URL === undefined) {
+    return;
+  }
+  const res = await axios.get(API_URL);
+  if (res.status === 200) {
+    return res.data;
+  } else {
+    console.log(`error: status code is ${res.status}`);
+    return;
+  }
+}
