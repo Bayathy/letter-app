545bc44ffd15be759474b7135560de8e3caac726
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-16 21:42:39 +0900

    簡単なヘッダーの追加

---
+++ src/app/components/common/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="h-12 flex items-center justify-between pl-6 bg-white border-b border-gray-200">
+      <button>
+        <p className="text-xl font-bold">Letter App</p>
+      </button>
+    </div>
+  );
+}
+
+export default Header;
