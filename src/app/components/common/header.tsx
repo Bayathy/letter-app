50d645d5695646abd4d2fa5a7c9b434127d74ab6
Keito Kobayashi <68783260+Bayathy@users.noreply.github.com>
2023-09-17 05:42:34 +0900

    Update src/app/components/common/header.tsx

--- src/app/components/common/header.tsx
+++ src/app/components/common/header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="h-12 flex items-center justify-between pl-6 bg-white border-b border-gray-200">
+    <header className="h-12 flex items-center justify-between pl-6 bg-white border-b border-gray-200">
       <button>
         <p className="text-xl font-bold">Letter App</p>
       </button>
