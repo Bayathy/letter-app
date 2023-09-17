087060a6012b126c09cf468e706326d7fee4f145
Bayathy <misosoup0321@icloud.com>
2023-09-17 15:25:21 +0900

    feat: 環境変数でOGPのurlを変更

--- src/app/components/share/share-editor.tsx
+++ src/app/components/share/share-editor.tsx
@@ -29,40 +29,37 @@ export const ShareEditor = () => {
 
   // はじめに描画を発火
   useEffect(() => {
-    if(!signaturePad) readyPad();
+    if (!signaturePad) readyPad();
 
-    const initShareEditor = async() => {
+    const initShareEditor = async () => {
       if (id === null) return;
       const strokes = await getStrokesByID(id);
-      draw(strokes)
-    }
+      draw(strokes);
+    };
     initShareEditor();
   }, [signaturePad]);
-  
+
   const draw = async (strokes: PointGroup[]) => {
     if (!canvasRef.current) return;
-    if (!signaturePad) return 
+    if (!signaturePad) return;
 
     let beforeTime = strokes[0].points[0].time;
-    
-    for(const stroke of strokes) {
-      const beforeStroke = signaturePad.toData();
-      const nowStroke = { ...stroke }
-      nowStroke.points = []
 
-      for(let j = 0; j < stroke.points.length; j++) {
-        const waitTime = stroke.points[j].time - beforeTime
+    for (const stroke of strokes) {
+      const beforeStroke = signaturePad.toData();
+      const nowStroke = { ...stroke };
+      nowStroke.points = [];
+
+      for (let j = 0; j < stroke.points.length; j++) {
+        const waitTime = stroke.points[j].time - beforeTime;
         nowStroke.points.push(stroke.points[j]);
-          signaturePad.fromData([
-            ...beforeStroke,
-            nowStroke
-        ] as PointGroup[])
-        await sleep(waitTime)
-        beforeTime = stroke.points[j].time
+        signaturePad.fromData([...beforeStroke, nowStroke] as PointGroup[]);
+        await sleep(waitTime);
+        beforeTime = stroke.points[j].time;
       }
     }
   };
-  
+
   return (
     <div className="grid w-full place-content-center gap-2" id="signature-pad">
       <canvas
