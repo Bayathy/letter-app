8d3802d8199cb07523c084a2d3812e7ae7490161
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-17 15:09:58 +0900

    とりあえずURLだけ書いてあるダイアログの追加

---
+++ src/app/components/editor/share-url-dialog.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import type { Dispatch, SetStateAction } from "react";
+
+export const ShareUrlDialog = ({letterId, setIsDialog}: {letterId: string, setIsDialog: Dispatch<SetStateAction<boolean>>}) => {
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div className="rounded-lg bg-white p-4 shadow-md">
+            <div className="flex items-center justify-between border-b-2 border-gray-200 pb-2">
+                <h2 className="text-xl font-semibold">シェア用URL</h2>
+                <button className="text-gray-500 hover:text-gray-700" id="closeButton" onClick={() => {setIsDialog(false)}}>
+                    <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button>
+            </div>
+            <div className="mt-4">
+                <p>{`https://letter-app-pi.vercel.app/share?letter_id=${letterId}`}</p>
+            </div>
+        </div>
+    </div>
+  );
+}
