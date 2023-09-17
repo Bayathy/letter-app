256ef17083dda8ed6b36a54165f471147e07e2b8
Bayathy <misosoup0321@icloud.com>
2023-09-17 16:02:10 +0900

    feat: UIの修正

--- src/app/components/preview/preview.tsx
+++ src/app/components/preview/preview.tsx
@@ -7,10 +7,8 @@ import { ShareUrlDialog } from "../editor/share-url-dialog";
 
 import type { PointGroup } from "signature_pad";
 
-
 import { previewCanvasAtom } from "@/app/store/preview-store";
 import { postStroke } from "@/services/stroke";
-
 
 export const Preview = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -38,7 +36,7 @@ export const Preview = () => {
     if (!canvasRef.current) return;
     const data = siganturePad?.toData();
     const res = await postStroke(data as PointGroup[]);
-    setUUID(res.letter_id)
+    setUUID(res.letter_id);
     setIsDialog(true);
   };
 
@@ -48,28 +46,24 @@ export const Preview = () => {
   }, []);
 
   return (
-    <div>
-      <div className="my-4 flex items-center justify-between gap-4 px-8">
-        <p className="font-bold">プレビュー</p>
-        <div className="flex gap-2">
-          <button
-            className="rounded-lg bg-blue-400 px-4 py-2 text-white"
-            onClick={handleClear}
-          >
-            Clear
-          </button>
-          <button
-            className="rounded-lg bg-blue-400 px-4 py-2 text-white"
-            onClick={handleShare}
-          >
-            Share
-          </button>
-        </div>
+    <div className="my-4 flex items-center justify-between gap-4 px-8">
+      <p className="font-bold">プレビュー</p>
+      <div className="flex gap-2">
+        <button
+          className="rounded-lg bg-blue-400 px-4 py-2 text-white"
+          onClick={handleClear}
+        >
+          Clear
+        </button>
+        <button
+          className="rounded-lg bg-blue-400 px-4 py-2 text-white"
+          onClick={handleShare}
+        >
+          Share
+        </button>
+      </div>
 
-      {
-        isDialog &&
-        <ShareUrlDialog letterId={uuid} setIsDialog={setIsDialog} />
-      }
+      {isDialog && <ShareUrlDialog letterId={uuid} setIsDialog={setIsDialog} />}
       <div className="my-4 flex justify-end gap-4 px-8">
         <button
           className="rounded-lg bg-blue-400 px-4 py-2 text-white hover:bg-blue-500"
@@ -83,7 +77,6 @@ export const Preview = () => {
         >
           Share
         </button>
-
       </div>
       <canvas
         className="mx-auto rounded-lg border-2 border-gray-400"
